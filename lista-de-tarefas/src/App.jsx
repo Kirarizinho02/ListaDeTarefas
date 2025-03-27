9433fd2e2dcd3ebd916cfce92f19594cfdcfaaca
SAMUEL DE OLIVEIRA AUGUSTO <samuel.augusto@etec.sp.gov.br>
2025-03-27 15:39:26 -0300

    Código Atualizado, faltando estilização e funções adicionais

---
+++ lista-de-tarefas/src/App.jsx
@@ -0,0 +1,15 @@
+import ListaTarefas from "./components/ListaTarefa";
+
+import './App.css';
+
+function App(){
+
+  return(
+    <>
+    <h1> Gerenciador de Tarefas </h1>
+    <ListaTarefas />
+    </>
+  );
+}
+
+export default App;
